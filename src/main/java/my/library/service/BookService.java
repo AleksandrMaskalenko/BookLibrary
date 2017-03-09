b33e62a943d4b2f76c29efebcf1d0987b8a378cc
Aleksandr Maskalenko <maskalenkoaleksandr@gmail.com>
2017-03-09 17:11:46 +0200

    1

---
+++ src/main/java/my/library/service/BookService.java
@@ -0,0 +1,15 @@
+package my.library.service;
+
+
+import my.library.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    public void addBook(Book book);
+    public void updateBook(Book book);
+    public void removeBook(int id);
+    public Book getBookById(int id);
+    public List<Book> listBooks();
+}
